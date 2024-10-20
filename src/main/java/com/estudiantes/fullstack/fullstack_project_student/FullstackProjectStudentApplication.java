2f8e46432c516ccf7cb8def3a74262c43bc4750f
ChristianRey117 <51842236+ChristianRey117@users.noreply.github.com>
2024-10-20 13:43:19 -0600

    init project

---
+++ src/main/java/com/estudiantes/fullstack/fullstack_project_student/FullstackProjectStudentApplication.java
@@ -0,0 +1,13 @@
+package com.estudiantes.fullstack.fullstack_project_student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackProjectStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackProjectStudentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudiantes/fullstack/fullstack_project_student/FullstackProjectStudentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudiantes.fullstack.fullstack_project_student;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackProjectStudentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
